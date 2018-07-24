193ef7a09f950dc947fc23129dd4b6dd2d400324
Sridhar Sathya <sridhar_0314@live.com>
2018-07-24 16:43:12 -0400

    Created class ServiceExtensions and added method ConfigureCors

---
+++ AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AccountOwnerServer.Extensions
+{
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// Configures  cors.
+        /// </summary>
+        /// <param name="service">The service.</param>
+        public static void ConfigureCors(this IServiceCollection service)
+        {
+            service.AddCors(o => o.AddPolicy("CorsPolicy",
+                b => b.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowCredentials()));
+        }
+    }
+}
