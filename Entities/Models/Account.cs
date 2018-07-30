f5419323e474d34e1661a1e19b436aa4c6912382
Sridhar Sathya <sridhar_0314@live.com>
2018-07-30 10:58:23 -0400

    Created project Entities Created model classes Account and Owner

---
+++ Entities/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
+{
+    public class Account
+    {
+        public Guid AccountId { get; set; }
+
+        [Required(ErrorMessage = "Date created is required")]
+        public DateTime DateCreated { get; set; }
+
+        [Required(ErrorMessage = "Account type is required")]
+        public string AccountType { get; set; }
+
+        [Required(ErrorMessage = "Owner Id is required")]
+        public Guid OwnerId { get; set; }
+    }
+}
---
+++ Entities/Models/Owner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
+{
+    public class Owner
+    {
+        public Guid OwnerId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(60, ErrorMessage = "Name too long (max 60 characters)")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth required")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(100, ErrorMessage = "Address too long (max 100 characters)")]
+        public string Address { get; set; }
+    }
+}
