287ca28e932f9186606c55cbf3516eb5dd9d0cdb
Sridhar Sathya <sridhar_0314@live.com>
2018-08-03 12:09:27 -0400

    Creation of services. Updated environment file to point to C# application based on environment. Created services environment & repository Environmet: reads env. variables and a common place to access it. Repository: a common module to access back-end services.

--- AccountOwnerClient/src/app/app.module.ts
+++ AccountOwnerClient/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
+import { EnvironmentUrlService } from './shared/services/environment-url.service';
+import { RepositoryService } from './shared/services/repository.service';
 
 @NgModule({
   declarations: [
@@ -16,14 +19,18 @@ import { NotFoundComponent } from './error-pages/not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    RepositoryService,
     RouterModule.forRoot([
-      {path: 'home', component: HomeComponent},
-      {path: '404', component: NotFoundComponent},
-      {path: '', redirectTo: '/home', pathMatch: 'full'},
-      {path: '**', redirectTo: '/404', pathMatch: 'full'}
+      { path: 'home', component: HomeComponent },
+      { path: '404', component: NotFoundComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', redirectTo: '/404', pathMatch: 'full' }
     ])
   ],
-  providers: [],
+  providers: [
+    EnvironmentUrlService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ AccountOwnerClient/src/app/shared/services/repository.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { EnvironmentUrlService } from './environment-url.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RepositoryService {
+  generateHeaders() {
+    return {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+  }
+  private createCompleteRoute(route: string, envAddress: string) {
+    return `${envAddress}/${route}`;
+  }
+
+  constructor(private http: HttpClient, private envUrl: EnvironmentUrlService) { }
+
+  public getDate(route: string) {
+    return this.http.get(this.createCompleteRoute(route, this.envUrl.urlAddress));
+  }
+  public create(route: string, body) {
+    return this.http.post(this.createCompleteRoute(route, this.envUrl.urlAddress),
+      body, this.generateHeaders());
+  }
+  public update(route: string, body) {
+    return this.http.put(this.createCompleteRoute(route, this.envUrl.urlAddress),
+      body, this.generateHeaders());
+  }
+  public delete(route: string) {
+    return this.http.delete(this.createCompleteRoute(route, this.envUrl.urlAddress));
+  }
+}
